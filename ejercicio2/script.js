aaf6f15f13cf1dfafb66d09c460e83abba60daf1
Elena Radu <lena@Air-de-Elena.home>
2022-04-25 19:31:36 +0200

    ejercicio 2 terminado

--- ejercicio2/script.js
+++ ejercicio2/script.js
@@ -1,28 +1,12 @@
 //2. Solicita al usuario una frase y una letra y muestra la cantidad de veces 
 //que aparece la letra en la frase.
 
-/*var frase = prompt ("Escribe una frase");
-var letra = prompt ("Dime una letra");
+const frase = prompt("Escribe una frase");
+const letra = prompt("Escribe una letra");
 
-var separacion = frase.split('');
+const separar = frase.split('');
+const encontrar = separar.filter(caracter => {
+    return caracter == letra
+});
 
-for(let i = 0; i <= separacion.length; i++) {
-    if(letra === separacion) {
-        var count = i++
-    }
-    console.log(count) 
-} 
-
-//------------------
-/*2
-var primero = prompt("Dime una frase")
-var segundo = prompt("Dime una letra")
-primero.split
-console.log(primero)
-
-if(segundo === primero){
-    primero.indexOf(segundo)
-    console.log(primero.indexOf(segundo))
-    alert(segundo)
-}
-*/
+alert(encontrar.length);
